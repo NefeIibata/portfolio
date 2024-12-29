7e6dc4cd6a6e4e4f9598ae8ebc26505999a60b91
nanokoroll321 <nanokoroll321@gmail.com>
2024-12-29 15:02:28 +0100

    feat: added smooth scroll and remove scroll bar

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,3 @@
-// document.querySelector('.about-left').addEventListener('mousemove', function (e) {
-//     let x = (e.clientX / window.innerWidth) - 0.5;
-//     let y = (e.clientY / window.innerHeight) - 0.5;
-
-//     const sphere = document.querySelector('.sphere');
-//     sphere.style.transform = `rotateX(${y * 20}deg) rotateY(${x * 20}deg)`;
-
-//     const icons = document.querySelectorAll('.icon');
-//     icons.forEach((icon, index) => {
-//         const offset = 40 + (index * 20); 
-//         icon.style.transform = `rotateY(calc(${index} * 40deg)) translateZ(${120 + offset}px) translateY(${y * 100 - offset}px)`;
-//     });
-// });
-
-
 document.querySelector('.about-left').addEventListener('mousemove', function (e) {
     let x = (e.clientX / window.innerWidth) - 0.5;
     let y = (e.clientY / window.innerHeight) - 0.5;
